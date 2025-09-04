4aa78ca3f4790d94a64b778078ce5b35ab7f21d3
JuliaRymem <juliakrasmusson@hotmail.com>
2025-09-04 10:50:58 +0200

    New files

---
+++ src/types/ClockSettings.ts
@@ -0,0 +1,1 @@
+// Övergripande klock-inställningar 
---
+++ src/types/TimeZone.ts
@@ -0,0 +1,3 @@
+// TimeZone som string literal union för vanliga zoner
+// tillåter även "vilken som helst" IANA-sträng via ett brandat string-typtrick
+//Detta gör att man får autocomplete för vanliga zoner + kan skriva egna
