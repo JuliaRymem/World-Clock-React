d49588dedf3e03bce71b18fa5cf9083bf9e0baaa
JuliaRymem <juliakrasmusson@hotmail.com>
2025-09-04 19:58:36 +0200

    display city name via props

--- src/components/CityCard.tsx
+++ src/components/CityCard.tsx
@@ -1,13 +1,14 @@
 // CityCard – klickbart kort som öppnar detaljvy.
 
 import React from "react"
+import type { City } from "../types/City"
 
-export function CityCard() {
-  return (
-    <div className="card">
-      <p>CityCard</p>
-    </div>
-  )
-}
+export function CityCard({ city }: { city: City }) {
+    return (
+      <div className="card">
+        <div className="city-name">{city.name}</div>
+      </div>
+    )
+  }
 
 export default CityCard
