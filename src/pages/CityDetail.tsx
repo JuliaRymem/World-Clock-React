036d2b5aec3445fa921f063fe9b6a3ba6fdcbe08
JuliaRymem <juliakrasmusson@hotmail.com>
2025-09-05 14:45:54 +0200

    add CityDetail route skeleton

---
+++ src/pages/CityDetail.tsx
@@ -0,0 +1,11 @@
+// enkel sida
+import React from "react";
+
+export default function CityDetail() {
+  return (
+    <div className="container">
+      <h2>City Detail</h2>
+      <p>Kommer snart…</p>
+    </div>
+  );
+}
