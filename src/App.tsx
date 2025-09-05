74b44290dc8c3135b13d1e5afe6edab0e5086187
JuliaRymem <juliakrasmusson@hotmail.com>
2025-09-05 14:45:19 +0200

    add CityDetail route skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Home from "./pages/Home";
+import CityDetail from "./pages/CityDetail";
+
 export default function App() {
-  return <>
-    <h1>Hello World!</h1>
-  </>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/city/:id" element={<CityDetail />} />
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
+  );
 }
